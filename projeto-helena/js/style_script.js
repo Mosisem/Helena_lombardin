b5c71aae8e5571cca11cec7ff997d860cf0b731c
Mosi sebastião <mosijoao4002@gmail.com>
2024-11-09 20:51:59 -0300

    Add files via upload

---
+++ projeto-helena/js/style_script.js
@@ -0,0 +1,24 @@
+var animacao_humanas = window.document.getElementById("humanas")
+var animacao_exatas = window.document.getElementById("exatas")
+var animacao_botao = window.document.getElementById("button")
+
+function humanas_clique(){
+      if (animacao_humanas.style.height == "300px"){
+            animacao_humanas.style.height="190px"
+      }else{
+             animacao_humanas.style.height="300px"
+             animacao_humanas.insertAdjacentText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo libero voluptas repellendus sunt modi consequatur voluptatem, fuga incidunt, atque suscipit quos iure. Rem, sit laborum soluta saepe sed esse.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam incidunt nam commodi totam obcaecati laborum, deserunt quibusdam repellendus architecto ipsam ea excepturi aut amet odit quidem, accusamus autem, asperiores saepe."
+      }
+}
+
+function exatas_clique(){
+      if (animacao_exatas.style.height == "300px"){
+            animacao_exatas.style.height="190px"
+            animacao_botao.innerText="Ver menos"
+      }else{
+             
+             animacao_exatas.style.height="300px"
+             animacao_exatas.insertAdjacentText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo libero voluptas repellendus sunt modi consequatur voluptatem, fuga incidunt, atque suscipit quos iure. Rem, sit laborum soluta saepe sed esse.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam incidunt nam commodi totam obcaecati laborum, deserunt quibusdam repellendus architecto ipsam ea excepturi aut amet odit quidem, accusamus autem, asperiores saepe."
+      }
+}
+

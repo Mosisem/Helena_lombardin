c324bcce4b99dd07d19a2b5784b75dbb437647ba
Mosi sebastião <mosijoao4002@gmail.com>
2025-07-24 18:39:43 -0300

    Add files via upload

---
+++ Helena_lombardin-main/Helena_lombardin-main/js/mural.js
@@ -0,0 +1,39 @@
+const div_mural = window.document.getElementById('contente_n_txt')
+
+function n_clube(){
+    div_mural.style.paddingTop="40px"
+    div_mural.style.backgroundColor="black";
+    div_mural.style.color="white"
+    div_mural.style.transition="2s"
+    div_mural.innerText=" Hoje, dia 29 de novembro, será realizada uma atividade especial nos Clubes! Estes espaços são dedicados à exploração de interesses pessoais, ao desenvolvimento de habilidades criativas e ao fortalecimento de competências socioemocionais. Participar dos clubes é uma excelente oportunidade para sair da rotina das disciplinas tradicionais e se envolver em atividades que promovem a expressão individual, o trabalho em equipe e a cidadania ativa. Não perca essa chance de ampliar seu aprendizado de forma divertida e enriquecedora!"
+}
+
+function n_itinerario(){
+    div_mural.style.paddingTop="40px"
+    div_mural.style.backgroundColor="black";
+    div_mural.style.color="white"
+    div_mural.style.transition="2s"
+    div_mural.innerText=" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad deserunt reiciendis, dolore facilis deleniti labore itaque beatae nam quod voluptatibus! Aliquam velit amet perspiciatis sint nam quod qui odit?"
+}
+
+function n_pais(){
+    div_mural.style.paddingTop="40px"
+    div_mural.style.backgroundColor="black";
+    div_mural.style.color="white"
+    div_mural.style.transition="2s"
+    div_mural.innerText=" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad deserunt reiciendis, dolore facilis deleniti labore itaque beatae nam quod voluptatibus! Aliquam velit amet perspiciatis sint nam quod qui odit?"
+}
+
+function n_especiais(){
+    div_mural.style.paddingTop="40px"
+    div_mural.style.backgroundColor="black";
+    div_mural.style.color="white"
+    div_mural.style.transition="2s"
+    div_mural.innerText=" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad deserunt reiciendis, dolore facilis deleniti labore itaque beatae nam quod voluptatibus! Aliquam velit amet perspiciatis sint nam quod qui odit?"
+
+}
+
+function  reverse(){
+    div_mural.innerHTML=""
+    div_mural.style.backgroundColor="white"
+}
